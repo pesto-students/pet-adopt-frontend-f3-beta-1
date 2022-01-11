8014f59450b3eff32c4b95ba1d45d1ffa10a6b63
shank <learnreactt@protonmail.com>
2022-01-12 02:48:18 +0530

    completed add pet card

--- src/components/common/DisplayPetCard/DisplayPetCard.jsx
+++ src/components/common/DisplayPetCard/DisplayPetCard.jsx
@@ -13,11 +13,24 @@ function DisplayPetCard() {
               alt="pet"
             />
           </div>
-          <div>
-            <div>
-              <span>Name</span>
-              <span>Breed</span>
-              <span>Gender</span>
+          <div className={styles.card_details}>
+            <div className={styles.card_title}>
+              <span className={styles.card_title_name}>Max</span>
+              <span className={styles.card_title_breed}>Breed</span>
+              <span className={styles.card_title_gender}>Gender</span>
+            </div>
+            <div className={styles.card_paragraph}>
+              <p>
+                The German Shepherd personality is aloof but not usually
+                aggressive. They're reserved dogs.
+              </p>
+            </div>
+            <div className={styles.card_footer}>
+              <span className={styles.card_footer_amount}>5000</span>
+              <span className={styles.card_footer_location}>Location</span>
+              <span className={styles.card_footer_send_request}>
+                <span>Send Request</span>
+              </span>
             </div>
           </div>
         </div>
