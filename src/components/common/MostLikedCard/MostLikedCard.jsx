49f279a0893c30aea0d86cfb6ea31c47df0bc873
shank <learnreactt@protonmail.com>
2022-01-31 01:28:57 +0530

    PetPal most liked pets

--- src/components/common/MostLikedCard/MostLikedCard.jsx
+++ src/components/common/MostLikedCard/MostLikedCard.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import styles from "./MostLikedCard.module.css";
 import { Col } from "react-bootstrap";
 
-function MostLikedCard({
-  breedName = "German Sheperd",
-  breedDetails = "Personality is aloof but not usually aggressive.",
-}) {
+function MostLikedCard({ mostLiked }) {
+  let imageKey = mostLiked.petimages
+    ? mostLiked.petimages[0].image
+    : "1309b1565d06708b4a1660de6d7078de";
+
   return (
     <Col xs={12} md={4}>
-      <div className={styles.card}>
+      <div>
+        <img className={styles.card} src={"/images/" + imageKey} alt="pet" />
         <div>
-          <div className={styles.card_title}>{breedName}</div>
-          <div className={styles.card_title_paragraph}>{breedDetails}</div>
+          <div className={styles.card_title}>
+            {mostLiked.selectedPet || "German "}
+          </div>
+          <div className={styles.card_title_paragraph}>
+            {mostLiked.about || "lorem ipsum"}
+          </div>
         </div>
       </div>
     </Col>
--- src/components/pets/MostLikedPets/MostLikedPets.jsx
+++ src/components/pets/MostLikedPets/MostLikedPets.jsx
@@ -2,33 +2,28 @@ import React from "react";
 import MostLikedCard from "../../common/MostLikedCard/MostLikedCard";
 import styles from "./MostLikedPets.module.css";
 import { Row } from "react-bootstrap";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 
 function MostLikedPets() {
-  const state = useSelector(state=>state.petDetail)
-  console.log(state,"state");
+  const state = useSelector((state) => state.petDetail);
+  let mostLikedState = [...state].sort(compare);
+
   function compare(a, b) {
-    // Use toUpperCase() to ignore character casing
     const A = a.likes.length;
     const B = b.likes.length;
-    // console.log(bandA,bandB);
-    if (A > B) return 1;
-    if (B > A) return -1;
+    if (A > B) return -1;
+    if (B > A) return 1;
   }
 
-  // // const sortState = state.sort((a, b) => a.likes.length - b.likes.length)
-  // if(state.length!==0){
-  //   console.log(state[0].sort(compare),"After sorting");
-  // }
   return (
     <div>
       <div className={styles.grid_container_h1}>
         <h2>MostLikedCard</h2>
       </div>
       <Row className={styles.grid_container}>
-        <MostLikedCard />
-        <MostLikedCard />
-        <MostLikedCard />
+        <MostLikedCard mostLiked={mostLikedState[0] || "mostLiked"} />
+        <MostLikedCard mostLiked={mostLikedState[1] || "mostLiked"} />
+        <MostLikedCard mostLiked={mostLikedState[2] || "mostLiked"} />
       </Row>
     </div>
   );
