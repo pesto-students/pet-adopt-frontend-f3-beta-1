b3a99ab120728a5daf3e477a90252e0b08af0052
shank <learnreactt@protonmail.com>
2022-01-31 19:42:49 +0530

    added name and url

--- src/components/common/PopularCategoryCard/PopularCategoryCard.jsx
+++ src/components/common/PopularCategoryCard/PopularCategoryCard.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import styles from "./PopularCategoryCard.module.css";
 import { Col } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function PopularCategoryCard({category}) {
+function PopularCategoryCard({ category, url }) {
   return (
-    <Col  xs={12} md={4} >
+    <Col xs={12} md={4}>
       <Link to={`/category/${category}`}>
-      <div className={styles.popular_category_card}>
-        <div className={styles.popular_category_card_content}>
-          <div className={styles.popular_category_card_image}></div>
-          <div>
-            <div className={styles.popular_category_card_title}>{category}</div>
-            <div className={styles.popular_category_card_description}>
-              16700 Dogs
+        <div className={styles.popular_category_card}>
+          <div className={styles.popular_category_card_content}>
+            <img className={styles.popular_category_card_image} src={url} />
+            <div>
+              <div className={styles.popular_category_card_title}>
+                {category}
+              </div>
+              <div className={styles.popular_category_card_description}>
+                16700 Dogs
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </Link>
     </Col>
   );
